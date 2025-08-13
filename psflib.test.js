eb008b7dbff0bbb67c1b72f5851ffa5ea0b13cfb
Michael S. Kenniston <git@michaelkenniston.com>
2025-08-13 16:55:31 -0400

    added new tests and cleand up old tests

--- psflib.test.js
+++ psflib.test.js
@@ -27,69 +27,110 @@ const Distance = psflib.Distance;
 const SCALE_FACTORS = psflib.SCALE_FACTORS;
 const Point = psflib.Point;
 
-function expectDist(s) {
+function expectDV(s) {
   return expect(new Distance(s)._value);
 }
 
 describe("Distance", () => {
   test("single metric units work", () => {
-    expectDist("1 m").toBeCloseTo(1, 3);
-    expectDist("2 meter").toBeCloseTo(2, 3);
-    expectDist("3.5 meters").toBeCloseTo(3.5, 3);
-    expectDist("4 metre").toBeCloseTo(4, 3);
-    expectDist("-0.05 metres").toBeCloseTo(-0.05, 5);
-    expectDist("1.23 km").toBeCloseTo(1230);
-    expectDist("1.34 kilometer").toBeCloseTo(1340);
-    expectDist("2.34 kilometers").toBeCloseTo(2340);
-    expectDist("3.45 kilometre").toBeCloseTo(3450);
-    expectDist("4.567 kilometres").toBeCloseTo(4567);
-    expectDist("34.5 cm").toBeCloseTo(0.345, 4);
-    expectDist("23 centimeter").toBeCloseTo(0.23, 4);
-    expectDist("2 centimeters").toBeCloseTo(0.02, 5);
-    expectDist("0.43 centimetre").toBeCloseTo(0.0043, 6);
-    expectDist("0.001 centimetres").toBeCloseTo(0.00001, 8);
-    expectDist("146 mm").toBeCloseTo(0.146, 4);
-    expectDist("89 millimeter").toBeCloseTo(0.089, 5);
-    expectDist("3 millimeters").toBeCloseTo(0.003, 6);
-    expectDist("63 millimetre").toBeCloseTo(0.063, 5);
-    expectDist("1 millimetres").toBeCloseTo(0.001, 6);
+    expectDV("1 m").toBeCloseTo(1, 3);
+    expectDV("2 meter").toBeCloseTo(2, 3);
+    expectDV("3.5 meters").toBeCloseTo(3.5, 3);
+    expectDV("4 metre").toBeCloseTo(4, 3);
+    expectDV("-0.05 metres").toBeCloseTo(-0.05, 5);
+    expectDV("1.23 km").toBeCloseTo(1230);
+    expectDV("1.34 kilometer").toBeCloseTo(1340);
+    expectDV("2.34 kilometers").toBeCloseTo(2340);
+    expectDV("3.45 kilometre").toBeCloseTo(3450);
+    expectDV("4.567 kilometres").toBeCloseTo(4567);
+    expectDV("34.5 cm").toBeCloseTo(0.345, 4);
+    expectDV("23 centimeter").toBeCloseTo(0.23, 4);
+    expectDV("2 centimeters").toBeCloseTo(0.02, 5);
+    expectDV("0.43 centimetre").toBeCloseTo(0.0043, 6);
+    expectDV("0.001 centimetres").toBeCloseTo(0.00001, 8);
+    expectDV("146 mm").toBeCloseTo(0.146, 4);
+    expectDV("89 millimeter").toBeCloseTo(0.089, 5);
+    expectDV("3 millimeters").toBeCloseTo(0.003, 6);
+    expectDV("63 millimetre").toBeCloseTo(0.063, 5);
+    expectDV("1 millimetres").toBeCloseTo(0.001, 6);
   });
 
   test("single US units work", () => {
-    expectDist("2 inch").toBeCloseTo(0.0508, 5);
-    expectDist("3 inches").toBeCloseTo(0.0762, 5);
-    expectDist("4 in").toBeCloseTo(0.1016, 4);
-    expectDist('6"').toBeCloseTo(0.1524, 4);
-    expectDist("3.5 foot").toBeCloseTo(1.0668, 3);
-    expectDist("2 feet").toBeCloseTo(0.6096, 4);
-    expectDist("10 ft").toBeCloseTo(3.048, 3);
-    expectDist("4'").toBeCloseTo(1.2192, 3);
-    expectDist("0.4 yard").toBeCloseTo(0.36576, 4);
-    expectDist("1.7 yards").toBeCloseTo(1.55448, 3);
-    expectDist("5 yd").toBeCloseTo(4.572, 3);
-    expectDist("1 barleycorn").toBeCloseTo(0.00846667, 6);
-    expectDist("2 barleycorns").toBeCloseTo(0.0169333, 5);
-    expectDist("3 furlong").toBeCloseTo(603.504);
-    expectDist("4 furlongs").toBeCloseTo(804.672);
-    expectDist("5 chain").toBeCloseTo(100.584);
-    expectDist("6 chains").toBeCloseTo(120.701);
-    expectDist("7 rod").toBeCloseTo(35.204);
-    expectDist("8 rods").toBeCloseTo(40.2336);
-    expectDist("9 link").toBeCloseTo(1.809, 3);
-    expectDist("10 links").toBeCloseTo(2.01, 3);
-    expectDist("11 cubit").toBeCloseTo(5.0292, 3);
-    expectDist("12 cubits").toBeCloseTo(5.4864, 3);
-    expectDist("13 fathom").toBeCloseTo(23.7744);
-    expectDist("14 fathoms").toBeCloseTo(25.6032);
-    expectDist("15 league").toBeCloseTo(83340);
-    expectDist("16 leagues").toBeCloseTo(88896);
+    expectDV("2 inch").toBeCloseTo(0.0508, 5);
+    expectDV("3 inches").toBeCloseTo(0.0762, 5);
+    expectDV("4 in").toBeCloseTo(0.1016, 4);
+    expectDV('6"').toBeCloseTo(0.1524, 4);
+    expectDV("3.5 foot").toBeCloseTo(1.0668, 3);
+    expectDV("2 feet").toBeCloseTo(0.6096, 4);
+    expectDV("10 ft").toBeCloseTo(3.048, 3);
+    expectDV("4'").toBeCloseTo(1.2192, 3);
+    expectDV("0.4 yard").toBeCloseTo(0.36576, 4);
+    expectDV("1.7 yards").toBeCloseTo(1.55448, 3);
+    expectDV("5 yd").toBeCloseTo(4.572, 3);
+    expectDV("1 barleycorn").toBeCloseTo(0.00846667, 6);
+    expectDV("2 barleycorns").toBeCloseTo(0.0169333, 5);
+    expectDV("3 furlong").toBeCloseTo(603.504);
+    expectDV("4 furlongs").toBeCloseTo(804.672);
+    expectDV("5 chain").toBeCloseTo(100.584);
+    expectDV("6 chains").toBeCloseTo(120.701);
+    expectDV("7 rod").toBeCloseTo(35.204);
+    expectDV("8 rods").toBeCloseTo(40.2336);
+    expectDV("9 link").toBeCloseTo(1.809, 3);
+    expectDV("10 links").toBeCloseTo(2.01, 3);
+    expectDV("11 cubit").toBeCloseTo(5.0292, 3);
+    expectDV("12 cubits").toBeCloseTo(5.4864, 3);
+    expectDV("13 fathom").toBeCloseTo(23.7744);
+    expectDV("14 fathoms").toBeCloseTo(25.6032);
+    expectDV("15 league").toBeCloseTo(83340);
+    expectDV("16 leagues").toBeCloseTo(88896);
   });
 
-  test("multiple units and non-spaced work", () => {
-    expectDist('1 ft 3"').toBeCloseTo(0.381, 4);
+  test("example syntactic forms from doc all work", () => {
+    expectDV("1 m").toBeCloseTo(1, 3);
+    expectDV('2 m').toBeCloseTo(2, 3);
+    expectDV("3 m 46 cm").toBeCloseTo(3.46, 3);
+    expectDV("4 m 0 cm").toBeCloseTo(4.0, 3);
+    expectDV("5m62cm").toBeCloseTo(5.62, 3);
+    expectDV(`3' 6"`).toBeCloseTo(1.0668, 3);
+    expectDV(`${2*3} m`).toBeCloseTo(6, 3);
+    expectDV("4.56 m").toBeCloseTo(4.56, 3);
+    expectDV("-8 m").toBeCloseTo(-8, 3);
+    expectDV("+9 m").toBeCloseTo(9, 3);
+  });
+
+  test("variations on forms", () => {
+    expectDV(new Distance("387 m")).toBeCloseTo(387);
+    expectDV(undefined).toBe(undefined);
+  });
+
+  test("Distance.plus() works", () => {
+    const p = new Distance("3 m");
+    const z = new Distance("0 m");
+    const a = new Distance("  8   m   ");
+    expect(p.plus(a)._value).toBeCloseTo(11);
+    expect(p.plus(z)._value).toBeCloseTo(3);
+    expect(() => (p.plus())).toThrow();
+  });
+
+  test("Distance.times() works", () => {
+    const p = new Distance("11 m");
+    expect(p.times(0)._value).toBe(0);
+    expect(p.times(1)._value).toBeCloseTo(11);
+    expect(p.times(10)._value).toBeCloseTo(110);
+    expect(p.times(-2)._value).toBeCloseTo(-22);
+    expect(() => (p.times("foo"))).toThrow();
+    expect(() => (p.times())).toThrow();
   });
 
   test("illegal formats detected", () => {
+    expect(() => (new Distance(5))).toThrow();
+    expect(() => (new Distance("4 m cm"))).toThrow();
+    expect(() => (new Distance("3 * 8 m"))).toThrow();
+    expect(() => (new Distance("m 8"))).toThrow();
+    expect(() => (new Distance("10 toes"))).toThrow();
+    expect(() => (new Distance("0 m").plus(2))).toThrow();
+    expect(() => (new Distance("3 m").plus())).toThrow();
+    expect(() => (new Distance("2.5 m").times("5"))).toThrow();
   });
 });
 
@@ -114,37 +155,35 @@ describe("SCALE_FACTORS", () => {
 });
 
 describe("Point", () => {
-  const p1 = new Point(null, "10 in", "20 in");
-  const p2 = new Point(null, "3 ft 5 in", "1 ft 7 in");
+  const p1 = new Point("dummy", "10 ft", "20 in");
+  const p2 = new Point(null, new Distance("3 ft 5 in"),
+                             new Distance( "1 ft 7 in"));
 
-  test("point is created", () => {
+  test("Point.constructor works", () => {
     expect(p1).toBeInstanceOf(Point);
-  });
-
-  test("input coordinates available", () => {
-    expect(p1.inX()._value).toBeCloseTo(0.254, 4);
+    expect(p1._parent).toBe("dummy");
+    expect(p1.inX()._value).toBeCloseTo(3.048, 3);
     expect(p1.inY()._value).toBeCloseTo(0.508, 4);
+    expect(p1._outX).toBe(null);
+    expect(p1._outY).toBe(null);
   });
 
-  test("feet-and-inches working", () => {
+  test("Point.move() works", () => {
     expect(p2.inX()._value).toBeCloseTo(1.0414, 3);
     expect(p2.inY()._value).toBeCloseTo(0.4826, 4);
     var p3 = p1.move("3 ft -2 in", "4 ft 0 in");
-    expect(p3.inX()._value).toBeCloseTo(1.1176, 3);
+    expect(p3.inX()._value).toBeCloseTo(3.9116, 3);
     expect(p3.inY()._value).toBeCloseTo(1.7272);
-  });
-
-  test("move() produces correct new Point", () => {
-    var p3 = p1.move("33 in", "48 in");
-    expect(p3.inX()._value).toBeCloseTo(1.0922, 3);
+    p3 = p1.move("33 in", "48 in");
+    expect(p3.inX()._value).toBeCloseTo(3.8862, 3);
     expect(p3.inY()._value).toBeCloseTo(1.7272, 3);
-    var p4 = p1.move("-4 in", "-12 in");
-    expect(p4.inX()._value).toBeCloseTo(0.1524, 4);
+    var p4 = p1.move(new Distance("-4 in"), new Distance( "-12 in"));
+    expect(p4.inX()._value).toBeCloseTo(2.9464, 3);
     expect(p4.inY()._value).toBeCloseTo(0.2032, 4);
     expect(p3._parent).toBe(p4._parent);
   });
 
-  test("output coordinates available", () => {
+  test("output coordinates available", () => {  // FIX ME
     expect(p1.outX()._value).toBe(111);
     expect(p1.outY()._value).toBe(222);
   });
